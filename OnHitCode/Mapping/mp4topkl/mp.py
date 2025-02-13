23615fd6357f90f68687a004ec5ed2b182ee7652
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-02-13 16:26:27 +1100

    basic inference done on testvideo.mp4

--- OnHitCode/Mapping/mp4topkl/mp.py
+++ OnHitCode/Mapping/mp4topkl/mp.py
@@ -9,16 +9,27 @@ mp_pose = mp.solutions.pose
 mp4_path = "/Users/natha/Desktop/on-hit/OnHitCode/Mapping/mp4topkl/TestVideo.mp4" # must use absolute path
 cap = cv2.VideoCapture(mp4_path) # have to use 1 for index for Mac Camera
 
-while cap.isOpened():
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
 
-    ret, frame = cap.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    cv2.imshow("Mediapipe Feed", frame)
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break 
+        results = pose.process(image)
 
-cap.release()
-cv2.destroyAllWindows()
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        print(results)
+
+        cv2.imshow("Mediapipe Feed", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break 
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
