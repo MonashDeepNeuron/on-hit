8b938cb3b5bce49393b87fc50b938c4e426e18f0
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-02-16 15:43:18 +1100

    updated changes to suit class structure

--- OnHitCode/Mapping/mp4topkl/mp4-to-inference.py
+++ OnHitCode/Mapping/mp4topkl/mp4-to-inference.py
@@ -2,14 +2,40 @@ import cv2
 import mediapipe as mp 
 import numpy as np 
 
-class mp4_to_mediapipe():
-
-    MP_POSE = mp.solutions.pose 
+class TrainingVideos():
+    
+    MP_to_NTU = {
+        None : 1 # need to create custom calculations based on the 2 hip positions
+        2
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+        11
+        12
+        13
+        14
+        15
+        16
+        17
+        18
+        19
+        20
+        21
+        22
+        23
+        24
+        25
+    }
     
     def __init__(self, video_path: str):
         self.video_path = video_path
     
-    def mediapipe_inference(self, min_detection_threshold: int, min_tracking_threshold: int) -> str:
+    def mediapipe_formatting(self, min_detection_threshold: int, min_tracking_threshold: int) -> str:
         """
         Returns a nested dictionary which has the 33 landmarks - each landmark corresponds to its own dictionary
         this dictionary has the x,y,z coordinates and their visibility 
@@ -17,7 +43,6 @@ class mp4_to_mediapipe():
 
         cap = cv2.VideoCapture(self.video_path)
         MP_POSE = mp.solutions.pose 
-
 
         with MP_POSE.Pose(min_detection_confidence=min_detection_threshold, min_tracking_confidence=min_tracking_threshold) as pose: 
 
@@ -43,8 +68,6 @@ class mp4_to_mediapipe():
                 print(results.pose_landmarks) # dictionary with index as the landmarks (0-32), has a nested dictionary with x,y,x and confidence
 
             cap.release()
-
-
 
 
 mp4_path = "/Users/natha/Desktop/on-hit/OnHitCode/Mapping/mp4topkl/TestVideo.mp4" 
