5cb95ef55b1c6deea97c4853d1bf952bd478357d
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-02-24 14:23:48 +1100

    started training optimiser

--- OnHitCode/Mapping/zedtopkl/optimiser.py
+++ OnHitCode/Mapping/zedtopkl/optimiser.py
@@ -1,15 +1,17 @@
-import 
+import os 
+
 class TrainingOptimiser():
 
     def __init__(self, input_folder: str):
         self.json = input_folder
-
+    
     def training_distributor(self, val: float, test: float, train: float) -> None:
         training_files = os.listdir(self.json)
 
-        print(training_files)
+        
 
-input_json_folder = "OnHitCode/Mapping/zedtopkl/json_clips"
+
+input_json_folder = "json_clips"
 
 optimiser = TrainingOptimiser(input_json_folder)
 optimiser.training_distributor(0.7, 0.2, 0.1)
