a6c95a03b9c920276cbdd3162d06fa661a9322bf
Nathan Lam <Nathan.lam.rt@gmail.com>
2025-05-16 19:14:43 +1000

    Adding data to the mongoDB ~ data cannot be posted to central loc yet

---
+++ backend/mongoTest.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient 
+import datetime 
+
+# TODO: Docker to ensure everyone has the same dependencies to post data to mongodb
+# TODO: Ensuring that everyone is posting to the same database so we have a central location 
+# TODO: Developing the test date 
+
+uri = 'mongodb://localhost:27017'
+client = MongoClient() # might need a URI
+
+db = client["test"] 
+collection = db["posts"]
+
+document = { 
+            "name" : "Nathan", 
+            "age" :  19, 
+            "city" : "Melbourne", 
+            } 
+
+document_2 = { 
+            "name" : "Lil Ninja", 
+            "age" :  79, 
+            "Dih Size": 100000000000,
+            } 
+
+result = collection.insert_one(document)
+result = collection.insert_one(document_2)
+print(result.inserted_id)
+
